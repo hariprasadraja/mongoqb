236ed3c68c71b347121744875fd447f1b029df5c
hariprasad <hariprasadcsmails@gmail.com>
2018-04-22 13:35:03 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,49 @@
+# mongoqb
+
+mongoqb is a versetaile query builder for mongodb from http request built in golang.  
+
+  - do a Request like http://localhost:9000?filterBy=name(and),emailId(or),age&name=hari,rahul,krishnan&emailId=hariprasadcsmails@gmail.com&age=24,
+
+  - get a mgo query as bson.M
+  bson.M{
+				"name":            bson.M{"$and": []string{"hari", "rahul", "krishnan"}},
+				"contact.emailId": bson.M{"$or": []string{"hariprasadcsmails@gmail.com"}},
+				"personal.age":    "24",
+			},
+  - Magic
+
+# Features!
+  - Build mongodb query from http request. 
+  - Conditional operators in mongodb are allowed. 
+  - Custom functions to do operations before making mongodb query.
+  - Only exported keys are allowed to transform into mongodb query. 
+
+### Dependencies
+mongoqb uses only one source projects to work properly:
+* [mgo] - a powerfull golang mongodb driver. 
+
+
+And of course mongodb itself is open source with a [public repository][dill]
+ on GitHub.
+
+### Installation
+mongoqb requires [mgo](https://labix.org/mgo) v2 to run.
+
+Install the dependencies by the following
+
+To install dependencies, run the below 
+
+```sh
+$ go get gopkg.in/mgo.v2
+```
+
+### Development
+
+Want to contribute? Great!
+contact me @ hariprasadcsmails@gmail.com
+
+**Free Software, Hell Yeah!**
+
+[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+
+   [mgo]: <http://labix.org/mgo>
---
+++ test/queryBuilder_test.go
@@ -0,0 +1,106 @@
+// This file provides utility tools to parse the query params from the request.
+// It provides a standardised approach to read the data from request query.
+// version: 0.1
+
+package test
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+
+	"gopkg.in/mgo.v2/bson"
+    "mongoqb/qb"
+)
+
+func TestBuildQuery(t *testing.T) {
+	type args struct {
+		request     *http.Request
+		allowedkeys map[string]qb.Options
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bson.M
+		wantErr bool
+		url     string
+	}{
+		{
+			name: "examine single filter params",
+			args: args{
+				allowedkeys: map[string]qb.Options{
+					"name": qb.Options{
+						DBPath: "name",
+					},
+				},
+			},
+			want: bson.M{
+				"name": "hari",
+			},
+			wantErr: false,
+			url:     "http://localhost:9000?filterBy=name&name=hari",
+		},
+		{
+			name: "examine multiple filter params",
+			args: args{
+				allowedkeys: map[string]qb.Options{
+					"name": {
+						DBPath: "name",
+					},
+					"emailId": {
+						DBPath: "contact.emailId",
+					},
+					"age": {
+						DBPath: "personal.age",
+					},
+				},
+			},
+
+			want: bson.M{
+				"name":            "hari",
+				"contact.emailId": "hariprasadcsmails@gmail.com",
+				"personal.age":    "24",
+			},
+			wantErr: false,
+			url:     "http://localhost:9000?filterBy=name,emailId,age&name=hari&emailId=hariprasadcsmails@gmail.com&age=24",
+		},
+		{
+			name: "examine logical operators in filter params",
+			args: args{
+				allowedkeys: map[string]qb.Options{
+					"name": {
+						DBPath: "name",
+					},
+					"emailId": {
+						DBPath: "contact.emailId",
+					},
+					"age": {
+						DBPath: "personal.age",
+					},
+				},
+			},
+			want: bson.M{
+				"name":            bson.M{"$and": []string{"hari", "rahul", "krishnan"}},
+				"contact.emailId": bson.M{"$or": []string{"hariprasadcsmails@gmail.com"}},
+				"personal.age":    "24",
+			},
+			wantErr: false,
+			url:     "http://localhost:9000?filterBy=name(and),emailId(or),age&name=hari,rahul,krishnan&emailId=hariprasadcsmails@gmail.com&age=24",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			tt.args.request, _ = http.NewRequest(http.MethodGet, tt.url, nil)
+			got, err := qb.BuildQuery(tt.args.request, tt.args.allowedkeys)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BuildQuery() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BuildQuery() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
